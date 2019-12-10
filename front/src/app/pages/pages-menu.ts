c2647b7a6eb648c7d0cd4bd2a0d9353f0c3978dd
Fábio Naspolini <fabionaspolini@gmail.com>
2019-12-10 00:56:22 -0300

    Menu ident

--- front/src/app/pages/pages-menu.ts
+++ front/src/app/pages/pages-menu.ts
@@ -1,46 +1,23 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  {
-    title: 'Dashboard',
-    icon: 'home-outline',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'FEATURES',
-    group: true,
-  },
+  {title: 'Dashboard', icon: 'home-outline', link: '/pages/dashboard', home: true},
+  {title: 'FEATURES', group: true},
   {
     title: 'Cadastros',
     icon: 'layers',
     children: [
-      {
-        title: 'Cidades',
-        link: '/pages/cadastros/cidade',
-      },
+      {title: 'Cidades', link: '/pages/cadastros/cidade'},
     ],
   },
   {
     title: 'Auth',
     icon: 'lock-outline',
     children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
+      {title: 'Login', link: '/auth/login'},
+      {title: 'Register', link: '/auth/register'},
+      {title: 'Request Password', link: '/auth/request-password'},
+      {title: 'Reset Password', link: '/auth/reset-password'},
     ],
   },
 ];
